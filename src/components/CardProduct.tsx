b216f249b5598aa0dbb5ed11d9c26fe61843b0d0
unknown <nazairendouffou@gmail.com>
2024-01-25 16:48:44 +0000

    manage some things to front into the cardProduct component and Categorie

--- src/components/CardProduct.tsx
+++ src/components/CardProduct.tsx
@@ -1,20 +1,32 @@
+import { useState } from "react";
 import productImage from "../assets/mixa.png"
 import "../styles/CardProduct.css"
-function CardProduct(){
-    return(
-        <div className="cardProduct">
-            <div className="containerImgProduct">
-                <img src={productImage} alt="" className="imgProduct"/>
-            </div>
-            <div>
-                <p className="titleProduct">Mixa</p>
-                <p className="indicationProduct">Pommade de bébé</p>
-                <p className="commentProduct">Hydrate et adoucie la peau</p>
-                <p>70 ml</p>
-            </div>
-            <div>
-                <p>2000 FCFA</p>
-            </div>
+function CardProduct() {
+    const [isClick, setIsClick] = useState(false);
+
+    return (
+        <div className="cardProduct" onClick={() => { setIsClick(!isClick); console.log(isClick) }}>
+            {!isClick ? (
+                <div>
+                    <div className="containerImgProduct">
+                        <img src={productImage} alt="" className="imgProduct" />
+                    </div>
+                    <div>
+                        <p className="titleProduct">Mixa</p>
+                        <p className="indicationProduct">Pommade de bébé</p>
+                        <p className="commentProduct">Hydrate et adoucie la peau</p>
+                        <div className="containerCapacity"><p>70 ml</p></div>
+                    </div>
+                    <div className="priceContainer">
+                        <p className="prix">2000 FCFA</p>
+                    </div>
+                </div>) : (<div className="ContainerVersoCard">
+                    <p>Pharmacie</p>
+                    <p>Christ Stella</p>
+                    <p>Attécoubé</p>
+                    <p className="voirPlusButton">Voir +</p>
+                </div>)
+            }
         </div>
     )
 }
--- src/views/Beaute_Cosmetique.tsx
+++ src/views/Beaute_Cosmetique.tsx
@@ -3,49 +3,60 @@ import categorie from "../classes/categorie";
 import '../styles/Categorie.css'
 import CardProduct from "../components/CardProduct";
 
-function Beaute_Cosmetique(){
-    const categories= [
-        {id: 1, label: "Soin anti age, anti ride"},
-        {id: 2, label: "cheveux"},
-        {id: 3, label: "Douche"},
-        {id: 4, label: "Soin de visage"},
-        {id: 5, label: "Soin de corps"},
-        {id: 6, label: "Soin nettoyant visage"},
-        {id: 7, label: "Complément beauté"},
-        {id: 8, label: "Anti imperfections"},
-        {id: 9, label: "Produits hygiène intime"},
-        {id: 10, label: "parfum"},
-        {id: 11, label: "Bain"},
-        {id: 12, label: "Produits maquillage"},
-        {id: 13, label: "Déodorant"},
-        {id: 14, label: "Soins des pieds"},
-        {id: 15, label: "Soin des lèvres"},
-        {id: 16, label: "Savon"},
-        {id: 17, label: "Démaquillants"},
-        {id: 19, label: "Soin des ongles"},
-        {id: 20, label: "Soin homme"},
-        {id: 21, label: "Epilation"},
-        {id: 22, label: "Masques visage"},
+function Beaute_Cosmetique() {
+    const categories = [
+        { id: 1, label: "Soin anti age, anti ride" },
+        { id: 2, label: "cheveux" },
+        { id: 3, label: "Douche" },
+        { id: 4, label: "Soin de visage" },
+        { id: 5, label: "Soin de corps" },
+        { id: 6, label: "Soin nettoyant visage" },
+        { id: 7, label: "Complément beauté" },
+        { id: 8, label: "Anti imperfections" },
+        { id: 9, label: "Produits hygiène intime" },
+        { id: 10, label: "parfum" },
+        { id: 11, label: "Bain" },
+        { id: 12, label: "Produits maquillage" },
+        { id: 13, label: "Déodorant" },
+        { id: 14, label: "Soins des pieds" },
+        { id: 15, label: "Soin des lèvres" },
+        { id: 16, label: "Savon" },
+        { id: 17, label: "Démaquillants" },
+        { id: 19, label: "Soin des ongles" },
+        { id: 20, label: "Soin homme" },
+        { id: 21, label: "Epilation" },
+        { id: 22, label: "Masques visage" },
     ]
 
-    const renderItem = (item: categorie, idx:any) => (
+    const renderItem = (item: categorie, idx: any) => (
         <div key={idx} className="element">
             <a href="">{item.label}</a>
         </div>
-      );
+    );
 
-    return(
+    return (
         <div>
-        <h4>Beauté et cosmétique</h4>
-        <div className='categorie_container'>
-            <FlatList
-                list={categories}
-                renderItem={renderItem}
-                renderWhenEmpty={() => <div>No categories found</div>}
-            />
-        </div> 
-        <CardProduct/> 
-        </div>  
+            <h4>Beauté et cosmétique</h4>
+            <div className='categorie_container'>
+                <FlatList
+                    list={categories}
+                    renderItem={renderItem}
+                    renderWhenEmpty={() => <div>No categories found</div>}
+                />
+            </div>
+            <div className="containerCard">
+                <CardProduct />
+                <CardProduct />
+                <CardProduct />
+                <CardProduct />
+                <CardProduct />
+                <CardProduct />
+                <CardProduct />
+                <CardProduct />
+                <CardProduct />
+                <CardProduct />
+            </div>
+        </div>
     )
 }
 
